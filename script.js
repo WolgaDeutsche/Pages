a6a6e3ff9c4574469e642bab3ec0ad9d11baf92d
Lorenhow <105514629+Lorenhow@users.noreply.github.com>
2023-12-28 04:25:23 +0300

    b16.6fixed
    Accordeon fix!
    DO!
    NOT!
    MOVE!
    IT!
    AWAY!

--- script.js
+++ script.js
@@ -73,17 +73,3 @@ function org() {
     }
   
     // Accordion open/close
-    var acc = document.getElementsByClassName("accordion");
-  var i;
-  
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      } 
-    });
-  }
